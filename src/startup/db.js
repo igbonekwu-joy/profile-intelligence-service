b2f186ddd697977803b2cc599ee3f27ff6e53d6a
Joy <joykenneth605@gmail.com>
2026-04-14 23:10:42 +0100

    added configuration for db

---
+++ src/startup/db.js
@@ -0,0 +1,10 @@
+const mongoose = require("mongoose");
+const winston = require("winston");
+const config = require("../config");
+
+const db = process.env.NODE_ENV == 'test' ? config.DB_TEST_URI : config.DB_URI;
+
+module.exports = function () {
+    mongoose.connect(db)
+    .then(() => winston.info(`Connected to ${db}`));
+} 
